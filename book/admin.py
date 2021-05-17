6e5a7112e247163d88a569299b5c798786de0b30
winters0727 <winters0727@gmail.com>
2021-05-17 21:25:21 +0900

    Create dockerfile

--- book/admin.py
+++ book/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Book, Chapter, ChapterComment
+from .models import Book, Chapter, ChapterComment, TemporaryChapter
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Chapter)
 admin.site.register(ChapterComment)
+admin.site.register(TemporaryChapter)
--- config/settings.py
+++ config/settings.py
@@ -177,15 +177,15 @@ REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'author.serializers.AuthorSerializer'
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 # AUTHENTICATION_BACKENDS = [
 #     'django.contrib.auth.backends.ModelBackend',
